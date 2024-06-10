81b73471985ac6c7bb5b82c7b2395d7163d40c58
Gabriel Refosco <gbrefosco@gmail.com>
2024-06-10 14:06:45 -0300

    feat: Add generic Comment model related to User

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
-from .models import User
+from .models import User, Comment
 
 def index(request):
 	user_list = User.objects.all()
-	user_list_string = [str(user) for user in user_list]
-	response = '<br>'.join(user_list_string)
+	response = ''
+	for user in user_list:
+		response += f'<h2>{user.name} ({user.age})</h2>'
+		comment_list = Comment.objects.all()
+		for comment in comment_list:
+			response += f'<p>{comment.text} (on {comment.created_at})</p>'
 
 	return HttpResponse(response)
